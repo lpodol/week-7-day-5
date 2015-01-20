8b4b9337128834c0a821a056c27b90c094747207
lpodol <lpodol@users.noreply.github.com>
2015-01-20 00:44:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var favorite_movies = ["Breaking Away", "Sunrise", "Times Square", "Sholay", "The Umbrellas of Cherbourg", "Dazed and Confused", "Metropolitan"]
+var favorite_movies = ["tt0076162", "tt0078902", "tt0018455", "tt0081635", "tt0073707", "tt0058450", "tt0106677", "tt010014"]
 
-favorite_movies.forEach(function(title) {
-    var safe_url = encodeURI(title);
-    var url = "http://omdbapi.com/?t=" + safe_url;
+favorite_movies.forEach(function(id) {
+    var safe_url = encodeURI(id);
+    var url = "http://omdbapi.com/?i=" + safe_url;
     console.log(url);
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
@@ -16,26 +16,29 @@ favorite_movies.forEach(function(title) {
         var div = document.createElement("div");
         body.appendChild(div);
 
-        var name = document.createElement("h3");
-        div.appendChild(name);
-        var title = parsed.Title;
-        var cap = title.toUpperCase();
-        name.innerHTML = cap + " " + "(" + parsed.Year + ")";
-    
         var img = document.createElement("img");
         div.appendChild(img);
         var poster = parsed.Poster;
         img.src = poster;
 
+        var name = document.createElement("p");
+        div.appendChild(name);
+        var title = parsed.Title;
+        var cap = title.toUpperCase();
+        name.innerHTML = cap + " " + "(" + parsed.Year + ")";
+        name.setAttribute("id", "title");
+
         var plot = document.createElement("p");
         div.appendChild(plot);
+        plot.setAttribute("id", "plot");
         plot.innerHTML = parsed.Plot + " Rated: " + parsed.Rated + ".";
 
         var credits = document.createElement("p");
         div.appendChild(credits);
-        credits.innerHTML = " Directed by: " + parsed.Director + ". Written by: " + parsed.Writer + ". Starring: " + parsed.Actors + "."; 
-    });
+        credits.setAttribute("id", "credits");
+        credits.innerHTML = " Directed by: " + parsed.Director + ". Written by: " + parsed.Writer + ". Starring: " + parsed.Actors + ".";        
+});
+
 
     xhr.send();
-
 });
